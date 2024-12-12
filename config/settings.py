4f02d77617530c614a1b19bb2f83363badcd960b
s1f102003111 <s1f102003111@iniad.org>
2024-12-12 11:34:59 +0900

    c

--- config/settings.py
+++ config/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+9e3%9g_y-)+!r41rp*+kv%sorh#-$tp)j^-_efgu!y%l-8g46'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '.pythonanywhere.com', 'musicinsights.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
