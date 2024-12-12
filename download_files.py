0caa78b968cf23ef37cdf172ae313bad118cc6ee
s1f102003111 <s1f102003111@iniad.org>
2024-12-12 22:18:55 +0900

    e

--- download_files.py
+++ download_files.py
@@ -13,18 +13,16 @@ def download_file(file_url, destination_path):
         print(f"{destination_path} already exists.")
 
 def download_files():
-    """必要なファイルをダウンロードする関数"""
-    # Google DriveのファイルIDを指定（共有リンクからIDを抽出）
     sample_url = "https://drive.google.com/uc?id=1Egf77YksEqd2sDLdTyjC__VNgmvJFqdg"
     npy_url = "https://drive.google.com/uc?id=1rZl3xUMDt4iePMYd0Tek1df4Orwm73Af"
 
-    # ダウンロード先のパスを指定
-    sample_pt_path = Path(settings.BASE_DIR, "myproject", "sample.pt")
-    model_npy_path = Path(settings.BASE_DIR, "myproject", "lyrics_fasttext_model.model.wv.vectors_ngrams.npy")
+    # Render 上のパスを直接指定
+    sample_pt_path = Path("/opt/render/project/src/myproject/sample.pt")
+    model_npy_path = Path("/opt/render/project/src/myproject/lyrics_fasttext_model.model.wv.vectors_ngrams.npy")
     
-    # 各ファイルをダウンロード
     download_file(sample_url, sample_pt_path)
     download_file(npy_url, model_npy_path)
+
 
 if __name__ == "__main__":
     download_files()
